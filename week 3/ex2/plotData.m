93bbe0908926911fac5acdfaad9d875268995d92
浩淼 <phaomiao99mail@gmail.com>
2021-03-11 01:10:32 +0800

    add ex2

---
+++ week 3/ex2/plotData.m
@@ -0,0 +1,33 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+pos = find(y==1);   % find(X==n) 返回包含数组X中中所有满足值等于n元素的索引
+neg = find(y==0);
+
+plot(X(pos, 1), X(pos, 2), 'm+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'go', 'LineWidth', 2, 'MarkerSize', 7);
+
+
+
+
+
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
